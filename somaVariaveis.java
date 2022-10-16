22c3a8519203324651533500a21395ebfc9e3f28
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2022-10-15 22:25:05 -0300

    Create somaVariaveis.java

---
+++ somaVariaveis.java
@@ -0,0 +1,18 @@
+
+/**
+ * Atividade Colaborativa (3.03)
+ * 
+ * @author Hermes Feitoza Barros 
+ * @version 15/10/2022
+ */
+
+class somaVariaveis
+{
+    public static void main(String[] args){
+        int variavel1 = 5;
+        int variavel2 = 11;
+        int variavel3 = 28;
+        int variavelSoma = variavel1+variavel2+variavel3;
+        System.out.println("Soma dos valores: " + variavelSoma);
+    }
+}
